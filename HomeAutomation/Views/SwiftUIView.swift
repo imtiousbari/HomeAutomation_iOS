eb80751465d1f2451f364120c1c845f6739388b5
imtiousbari <mdimtiousbari@gmail.com>
2024-02-05 15:55:57 +0600

    up

--- HomeAutomation/Views/SwiftUIView.swift
+++ HomeAutomation/Views/SwiftUIView.swift
@@ -20,7 +20,7 @@ struct RoomsData: Decodable {
 
 struct SwiftUIView: View {
     @State private var rooms: [Room] = []
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Quick Access")
@@ -33,9 +33,9 @@ struct SwiftUIView: View {
             ScrollView(.vertical, showsIndicators: false) {
                 LazyVGrid(columns: [GridItem(.flexible(), spacing: 16), GridItem(.flexible(), spacing: 0)], spacing: 0) {
                     ForEach(rooms) { room in
-                                    RoomViewN(room: room)
-                                        .padding(8)
-                                }
+                        RoomViewN(room: room)
+                            .padding(8)
+                    }
                 }
                 
                 Spacer()
@@ -43,26 +43,26 @@ struct SwiftUIView: View {
             .padding()
         }
         .background(Color.Bg)
-//        VStack {
-//            ForEach(rooms) { room in
-//                RoomViewN(room: room)
-//                    .padding()
-//            }
-//        }
+        //        VStack {
+        //            ForEach(rooms) { room in
+        //                RoomViewN(room: room)
+        //                    .padding()
+        //            }
+        //        }
         .onAppear {
             loadData()
         }
     }
-
+    
     func loadData() {
         if let fileURL = Bundle.main.url(forResource: "QuickAcsRooms", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: fileURL)
                 let decoder = JSONDecoder()
                 let roomsData = try decoder.decode(RoomsData.self, from: data)
-
+                
                 rooms = roomsData.rooms
-
+                
                 // Print to verify data
                 print(rooms)
             } catch {
@@ -94,84 +94,84 @@ struct SwiftUIView: View {
 //        .shadow(radius: 5)
 //    }
 //}
-struct RoomViewN: View {
-    let room: Room
-    var body: some View {
-        if room.status == 1 {
-        Rectangle()
-        .frame(width: 170, height: 120) 
-                .foregroundColor(Color.Primary)
-                .cornerRadius(15)
-                .overlay(
-                    VStack {
-                        HStack {
-                            Image("4gangswthL")
-                                .resizable()
-                                .frame(width:20, height: 20)
-                            Spacer()
-                            Image(systemName: "power.circle")
-                                .font(.system(size: 20))
-                                .foregroundColor(.green)
-                                .onTapGesture {
-//                                    self.isSwitchOn.toggle()
-                                }
-                        }
-                        .padding(15)
-                        Spacer()
-                        HStack {
-                            Text(room.roomName)
-                                .foregroundColor( .white)
-                                .font(.system(size: 14))
-                            Spacer()
-                            Text("On")
-//                            Text(isSwitchOn ? "ON" : "OFF")
-                                .foregroundColor(Color.CusYellow)
-                                .font(.system(size: 14))
-                        }
-                        .padding()
-    //                    .padding(.bottom, 10)
-                    }
-                )
-        } else {
-            Rectangle()
-            .frame(width: 170, height: 120) 
-                .foregroundColor(Color.white)
-                .cornerRadius(15)
-                .overlay(
-                    VStack {
-                        HStack {
-                            Image("4gangswth")
-                                .resizable()
-                                .frame(width:20, height: 20)
-                            Spacer()
-                            Image(systemName: "power.circle")
-                                .font(.system(size: 20))
-                                .foregroundColor(.black)
-                                .onTapGesture {
-//                                    self.isSwitchOn.toggle()
-                                }
-                        }
-                        .padding(15)
-                        Spacer()
-                        HStack {
-                            Text(room.roomName)
-//                                .foregroundColor(isSwitchOn ? .white : .black)
-                                .font(.system(size: 12))
-                            Spacer()
-//                            Text(isSwitchOn ? "ON" : "OFF")
-//                                .foregroundColor(isSwitchOn ? Color.CusYellow : .green)
-                            Text("Off")
-//                            Text(isSwitchOn ? "ON" : "OFF")
-                                .foregroundColor(Color.red)
-                                .font(.system(size: 12))
-                        }
-                        .padding()
-    //                    .padding(.bottom, 10)
-                    }
-                )
-        }
-    }
-}
+//struct RoomViewN: View {
+//    let room: Room
+//    var body: some View {
+//        if room.status == 1 {
+//            Rectangle()
+//                .frame(width: 170, height: 120)
+//                .foregroundColor(Color.Primary)
+//                .cornerRadius(15)
+//                .overlay(
+//                    VStack {
+//                        HStack {
+//                            Image("4gangswthL")
+//                                .resizable()
+//                                .frame(width:20, height: 20)
+//                            Spacer()
+//                            Image(systemName: "power.circle")
+//                                .font(.system(size: 20))
+//                                .foregroundColor(.green)
+//                                .onTapGesture {
+//                                    //                                    self.isSwitchOn.toggle()
+//                                }
+//                        }
+//                        .padding(15)
+//                        Spacer()
+//                        HStack {
+//                            Text(room.roomName)
+//                                .foregroundColor( .white)
+//                                .font(.system(size: 14))
+//                            Spacer()
+//                            Text("On")
+//                            //                            Text(isSwitchOn ? "ON" : "OFF")
+//                                .foregroundColor(Color.CusYellow)
+//                                .font(.system(size: 14))
+//                        }
+//                        .padding()
+//                        //                    .padding(.bottom, 10)
+//                    }
+//                )
+//        } else {
+//            Rectangle()
+//                .frame(width: 170, height: 120)
+//                .foregroundColor(Color.white)
+//                .cornerRadius(15)
+//                .overlay(
+//                    VStack {
+//                        HStack {
+//                            Image("4gangswth")
+//                                .resizable()
+//                                .frame(width:20, height: 20)
+//                            Spacer()
+//                            Image(systemName: "power.circle")
+//                                .font(.system(size: 20))
+//                                .foregroundColor(.black)
+//                                .onTapGesture {
+//                                    //                                    self.isSwitchOn.toggle()
+//                                }
+//                        }
+//                        .padding(15)
+//                        Spacer()
+//                        HStack {
+//                            Text(room.roomName)
+//                            //                                .foregroundColor(isSwitchOn ? .white : .black)
+//                                .font(.system(size: 12))
+//                            Spacer()
+//                            //                            Text(isSwitchOn ? "ON" : "OFF")
+//                            //                                .foregroundColor(isSwitchOn ? Color.CusYellow : .green)
+//                            Text("Off")
+//                            //                            Text(isSwitchOn ? "ON" : "OFF")
+//                                .foregroundColor(Color.red)
+//                                .font(.system(size: 12))
+//                        }
+//                        .padding()
+//                        //                    .padding(.bottom, 10)
+//                    }
+//                )
+//        }
+//    }
+//}
 
 //struct SwitchView: View {
 //    @State private var isSwitchOn = false
@@ -220,6 +220,44 @@ struct RoomViewN: View {
 //    }
 //}
 
+struct RoomViewN: View {
+    let room: Room
+    
+    var body: some View {
+        Rectangle()
+            .frame(width: 170, height: 120)
+            .cornerRadius(15)
+            .overlay(
+                VStack {
+                    HStack {
+                        Image("4gangswth\(room.status == 1 ? "L" : "")")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                        Spacer()
+                        Image(systemName: "power.circle")
+                            .font(.system(size: 20))
+                            .foregroundColor(room.status == 1 ? .green : .black)
+                            .onTapGesture {
+                                // Handle tap gesture
+                            }
+                    }
+                    .padding(15)
+                    Spacer()
+                    HStack {
+                        Text(room.roomName)
+                            .foregroundColor(room.status == 1 ? .white : .black)
+                            .font(.system(size: 14))
+                        Spacer()
+                        Text(room.status == 1 ? "On" : "Off")
+                            .foregroundColor(room.status == 1 ? .CusYellow : .red)
+                            .font(.system(size: 14))
+                    }
+                    .padding()
+                }
+            )
+            .foregroundColor(room.status == 1 ? .Primary : .white)
+    }
+}
 
 
 #Preview {
